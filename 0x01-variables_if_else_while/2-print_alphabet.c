f90041e23d5a0abd16c7be041ce75d3534bb3a4f
aleeyugimba <babaali5154@gmail.com>
2022-12-12 13:59:03 -0800

    1-last_digit.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - prints alpha in lower case 
+ * main - prints alpha in lower case
  * Return: always 0
  */
 int main(void)
-{	
+{
 	char lc;
 
-	for (lc - 'a'; lc <= 'z'; lc++);
-	{	
+	for (lc - 'a'; lc <= 'z'; lc++)
+	{
 		putchar(lc);
 	}
 	putchar("\n");
