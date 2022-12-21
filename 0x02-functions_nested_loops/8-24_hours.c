b140c4032cb6d6a4cbfe5d2e373ca4ab3f041879
aleeyugimba <babaali5154@gmail.com>
2022-12-21 11:17:47 -0800

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,25 +1,32 @@
 #include "main.h"
 
 /**
- * jack_bauer -> prints every minute of the day
- *
+ * jack_bauer -> Prints every minute of the day of Jack Bauer
+ * Return: ...
  */
 void jack_bauer(void)
 {
-	int hour, minute;
+	int a, b, c, d;
 
-	for (hour = 0; hour  <= 23; hour++)
+	for (a = 0; a  <= 2; a++)
 	{
-		for (minute = 0; minute <= 59; minute++)
-		{
-
-			_putchar((hour / 10) + '0');
-			_putchar((hour % 10) + '0');
-			_putchar(':');
-			_putchar((minute / 10) + '0');
-			_putchar((minute %  0) + '0');
-			_putchar(('\n');
-		}
+	for (b  = 0; b  <= 9; b++)
+	{
+	if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
+	{
+	for (c = 0; c <= 5; c++)
+	{
+	for (d = 0; d <= 9; d++)
+	{
+	_putchar(a + '0');
+	_putchar(b + '0');
+	_putchar(58);
+	_putchar(c + '0');
+	_putchar(d + '0');
+	_putchar('\n');
 	}
-
+	}
+	}
+	}
+	}
 }
--- 0x02-functions_nested_loops/8-main.c
+++ 0x02-functions_nested_loops/8-main.c
@@ -1,11 +1,12 @@
 #include "main.h"
+
 /**
- * main - check the code
- *
- * Return: Always 0.
+* main - check the code
+*
+* Return: Always 0.
  */
-
 int main(void)
-
-	jack_bauer();
-	return (0);
+{
+    jack_bauer();
+    return (0);
+}
