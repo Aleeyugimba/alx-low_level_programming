a18ab1e706c0c43a37c38e44894cc4b5c451e31a
aleeyugimba <babaali5154@gmail.com>
2022-12-21 05:40:54 -0800

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -22,7 +22,7 @@ void times_table(void)
 	_putchar(32);
 	_putchar(d + '0');
 	_putchar(u + '0');
-	{
+	}
 	else
 	{
 	if (y != 0)
@@ -30,6 +30,8 @@ void times_table(void)
 	_putchar(44);
 	_putchar(32);
 	_putchar(32);
+	}
+	_putchar(z + '0');
 	}
 	}
 	_putchar('\n');
